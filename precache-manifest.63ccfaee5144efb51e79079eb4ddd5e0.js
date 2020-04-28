944255dee1fac52f7bdea921d9723cbc1530edb7
monicalaura <monicalauraburns@yahoo.com>
2020-04-28 12:15:01 +0300

    Updates

---
+++ precache-manifest.63ccfaee5144efb51e79079eb4ddd5e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774121f8c49580ecdb4c367fc3ffb6bc",
+    "url": "/monicalaura/quick-currency-converter/index.html"
+  },
+  {
+    "revision": "961ba4a199cd8d7777c4",
+    "url": "/monicalaura/quick-currency-converter/static/css/main.3c3c132e.chunk.css"
+  },
+  {
+    "revision": "894e1cf1c5643f4dc1f8",
+    "url": "/monicalaura/quick-currency-converter/static/js/2.46ca6d21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monicalaura/quick-currency-converter/static/js/2.46ca6d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "961ba4a199cd8d7777c4",
+    "url": "/monicalaura/quick-currency-converter/static/js/main.8caee644.chunk.js"
+  },
+  {
+    "revision": "b97dbc829390d970e0d3",
+    "url": "/monicalaura/quick-currency-converter/static/js/runtime-main.a3b0a184.js"
+  },
+  {
+    "revision": "dde67a68e9a534b0cdc77f024463e323",
+    "url": "/monicalaura/quick-currency-converter/static/media/back.dde67a68.jpg"
+  }
+]);
